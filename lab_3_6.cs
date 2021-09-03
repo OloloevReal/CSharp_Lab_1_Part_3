bbee4d734957ed03673b81f00ce432897ac73738
OloloevReal <nickey.n@gmail.com>
2021-09-04 02:14:29 +0300

    lab_3_6 done

--- lab_3_6.cs
+++ lab_3_6.cs
@@ -1,16 +1,38 @@
 using System;
 using System.Text;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab_1_Part_3
 {
     class Lab_3_6: ILab{
-        private readonly string inputS = @"";
+        private readonly string inputS = @"собака";
+
+        private char[,] items = {{'а', 'с', 'о'}, {'а', 'я', 'б'}, {'у', 'к', 'ц'}};
         public override void Do(){
             Console.WriteLine("Sourse data: ");
-            Console.WriteLine(inputS);
+            Utils.PrintArray(items);
+            Console.WriteLine($"Word: {inputS}");
             Console.WriteLine();
 
+            var list = items.Cast<char>().ToList();
+
+            bool CheckChars(){
+                if(inputS.Length > list.Count()) return false;
+
+                foreach(var w in inputS){
+                    if(!list.Remove(w))return false;
+                }
+                return true;
+            }
+
+            Console.Write($"Слово \"{inputS}\" составить ");
+            if(CheckChars()){
+                Console.WriteLine("возможно!");
+            }else{
+                Console.WriteLine("не получиться!");
+            }
+            
             Console.WriteLine();            
         }
         
--- utils.cs
+++ utils.cs
@@ -1,4 +1,20 @@
+using System;
+
 namespace Lab_1_Part_3
 {
-    
+    static class Utils
+    {
+        public static void PrintArray<T>(T[,] items)
+        {
+            for (int i = 0; i < items.GetLength(0); i++)
+            {
+                for (int j = 0; j < items.GetLength(1); j++)
+                {
+                    Console.Write("{0}\t", items[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
 }
