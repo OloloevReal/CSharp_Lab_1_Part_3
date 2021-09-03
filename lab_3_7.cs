75d278a623040fe2b185b8dca01a00d5fbdc0428
OloloevReal <nickey.n@gmail.com>
2021-09-04 02:44:34 +0300

    lab_3_7 done

--- lab_3_7.cs
+++ lab_3_7.cs
@@ -5,12 +5,39 @@ using System.Linq;
 namespace Lab_1_Part_3
 {
     class Lab_3_7: ILab{
-        private readonly string inputS = @"";
+        private readonly string inputS = @"Посланы    были к ним лазутчики.  Юлай,    крещеный калмык, сделал  коменданту важное донесение.";
         public override void Do(){
             Console.WriteLine("Sourse data: ");
             Console.WriteLine(inputS);
             Console.WriteLine();
 
+            /* var splitted = inputS.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            System.Text.StringBuilder StrB = new StringBuilder();
+            foreach(var s in splitted){
+                StrB.Append($"{s} ");
+            }
+            StrB.Remove(StrB.Length - 1, 1);
+            Console.WriteLine(StrB); */
+
+            char[] ch = inputS.ToCharArray();
+            char[] outChar = new char[ch.Length];
+
+            int id = 0;
+            bool skip = false;
+            for(int i = 0; i < ch.Length; i++){
+                if (ch[i] == ' '){
+                    if (!skip){
+                        skip = !skip;
+                        outChar[id++] = ' ';
+                    }
+                }else{
+                    skip = false;
+                    outChar[id++] = ch[i];
+                }
+            }
+            
+            Console.WriteLine("Result: ");
+            Console.WriteLine(new string(outChar,0, id));
             Console.WriteLine();            
         }
         
